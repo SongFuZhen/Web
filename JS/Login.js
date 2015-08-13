654cf306606490457b9086cc54543a1807559a2a
Songfuzhen <evil_song@126.com>
2015-08-13 17:43:05 +0800

    模具代码更新

---
+++ JS/Login.js
@@ -0,0 +1,28 @@
+/**
+ * Created by 宋福祯 on 15/8/13.
+ */
+function Login() {
+    var UserName = document.getElementById("Username").value;
+    var PassWord = document.getElementById("Password").value;
+    var FailLogin = document.getElementById("FailLogin");
+    var CheckUserName = document.getElementById('CheckUsername');
+    var CheckPassword = document.getElementById("CheckPassword");
+    var Fail = document.getElementById("Fail");
+
+    if (UserName == "") {
+        CheckUserName.innerHTML = "用户名不能为空";
+        setTimeout("CheckUserName.style.display='none'", 2000);
+    } else if (PassWord == "") {
+        CheckPassword.innerHTML = "密码不能为空";
+        setTimeout("CheckPassword.style.display='none'", 2000);
+    } else if (UserName == "admin" && PassWord == "1") {
+        FailLogin.style.display = "block";
+        document.getElementById("Fail").innerHTML = "登录成功";
+        //延时2秒进入
+        setTimeout("window.location.href = 'Main.html'", 1000);
+    } else if (FailLogin.style.display == "none") {
+        FailLogin.style.display = "block";
+        Fail.innerHTML = "用户名或密码错误";
+        setTimeout("FailLogin.style.display='none'", 2000);
+    }
+}
